4777a0a72fc8a3ea7dddae2484decaa15aacb45d
spitfirerara <nairaammara25@gmail.com>
2025-09-09 18:59:32 +0700

    Tugas 2 Individu PBP

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
 
-class Item(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = [
         ('jersey', 'Jersey'),
         ('shoes', 'Shoes'),
