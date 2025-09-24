1301d5e84729b438750c741fca01835b02b4cb6d
spitfirerara <nairaammara25@gmail.com>
2025-09-24 10:01:54 +0700

    Tugas Individu 4 PBP

--- realitea_club/settings.py
+++ realitea_club/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naira-ammara-realiteaclub.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://naira-ammara-realiteaclub.pbp.cs.ui.ac.id/"
+    "https://naira-ammara-realiteaclub.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
